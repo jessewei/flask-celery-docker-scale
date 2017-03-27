058cbb0344babb460b5c78c32fe1254b2807e29c
Jesse <web.jesse@gmail.com>
2017-03-28 07:00:31 +0800

    #1,flower fail to start, Add pip install pytz==2015.7

--- flask-celery/tasks.py
+++ flask-celery/tasks.py
@@ -15,4 +15,4 @@ celery= Celery('tasks',
 @celery.task(name='mytasks.add')
 def add(x, y):
     time.sleep(5) # lets sleep for a while before doing the gigantic addition task!
-    return x + y
+    return x * y
